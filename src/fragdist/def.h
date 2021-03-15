8cd6254ca2c76db20c0566802def26eafc51affd
Fedor Naumenko <30726965+fnaumenko@users.noreply.github.com>
2021-03-15 02:50:51 +0000

    Delete def.h

--- src/fragdist/def.h
+++
@@ -1,7 +0,0 @@
-#define OPT_DESCF_TSHIFT 3	// number of TABs before decsription field in Options::PrintUsage()
-
-#define	_FRAGDIST
-#define	_BAM
-#ifndef _DUP_OUTPUT
-#define _DUP_OUTPUT	// duplicated output
-#endif
